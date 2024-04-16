8771eb979ca646b9b9fbc05cffca0c19e10e9cdd
izotovaee <izotova.zg@gmail.com>
2024-04-16 15:44:04 +0300

    ConversationHandler_upd

--- main.py
+++ main.py
@@ -14,39 +14,31 @@ async def start(update, context):
         "Привет. Пройдите небольшой опрос, пожалуйста!\n"
         "Вы можете прервать опрос, послав команду /stop.\n"
         "В каком городе вы живёте?")
-
-    # Число-ключ в словаре states —
-    # втором параметре ConversationHandler'а.
     return 1
-    # Оно указывает, что дальше на сообщения от этого пользователя
-    # должен отвечать обработчик states[1].
-    # До этого момента обработчиков текстовых сообщений
-    # для этого пользователя не существовало,
-    # поэтому текстовые сообщения игнорировались.
 
 
+# Добавили словарь user_data в параметры.
 async def first_response(update, context):
-    # Это ответ на первый вопрос.
-    # Мы можем использовать его во втором вопросе.
-    locality = update.message.text
+    # Сохраняем ответ в словаре.
+    context.user_data['locality'] = update.message.text
     await update.message.reply_text(
-        f"Какая погода в городе {locality}?")
-    # Следующее текстовое сообщение будет обработано
-    # обработчиком states[2]
+        f"Какая погода в городе {context.user_data['locality']}?")
     return 2
 
 
+# Добавили словарь user_data в параметры.
 async def second_response(update, context):
-    # Ответ на второй вопрос.
-    # Мы можем его сохранить в базе данных или переслать куда-либо.
     weather = update.message.text
     logger.info(weather)
-    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
-    return ConversationHandler.END  # Константа, означающая конец диалога.
-    # Все обработчики из states и fallbacks становятся неактивными.
+    # Используем user_data в ответе.
+    await update.message.reply_text(
+        f"Спасибо за участие в опросе! Привет, {context.user_data['locality']}!")
+    context.user_data.clear()  # очищаем словарь с пользовательскими данными
+    return ConversationHandler.END
 
 
 async def stop(update, context):
+    context.user_data.clear()  # очищаем словарь с пользовательскими данными
     await update.message.reply_text("Всего доброго!")
     return ConversationHandler.END
 
@@ -54,20 +46,11 @@ async def stop(update, context):
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     conv_handler = ConversationHandler(
-        # Точка входа в диалог.
-        # В данном случае — команда /start. Она задаёт первый вопрос.
         entry_points=[CommandHandler('start', start)],
-
-        # Состояние внутри диалога.
-        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
         states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
             1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
             2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
         },
-
-        # Точка прерывания диалога. В данном случае — команда /stop.
         fallbacks=[CommandHandler('stop', stop)]
     )
 

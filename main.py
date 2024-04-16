ea53f1de1025eb2d8662a85ce1b07b22d24c6dd4
izotovaee <izotova.zg@gmail.com>
2024-04-16 15:35:04 +0300

    set_timer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import logging
-
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Application, CommandHandler
 from config import BOT_TOKEN
 
@@ -10,58 +8,60 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [['/address', '/phone'],
-                  ['/site', '/work_time']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+TIMER = 5  # таймер на 5 секунд
 
 
 async def start(update, context):
-    await update.message.reply_text(
-        "Я бот-справочник. Какая информация вам нужна?",
-        reply_markup=markup
-    )
+    """Объясняет как использовать бота."""
+    await update.message.reply_text('Привет. Использование: /set. Устанавливает таймер на 5 секунд.')
 
 
-async def close_keyboard(update, context):
-    await update.message.reply_text(
-        "Ok",
-        reply_markup=ReplyKeyboardRemove()
-    )
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
 
 
-async def help(update, context):
-    await update.message.reply_text(
-        "Я бот справочник.")
+async def task(context):
+    """Выводит сообщение"""
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! 5c. прошли!')
 
 
-async def address(update, context):
-    await update.message.reply_text(
-        "Адрес: г. Москва, ул. Льва Толстого, 16")
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    chat_id = update.effective_message.chat_id
+    # Добавляем задачу в очередь
+    # и останавливаем предыдущую (если она была)
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
+
+    text = f'Вернусь через 5 с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
 
 
-async def phone(update, context):
-    await update.message.reply_text("Телефон: +7(495)776-3030")
-
-
-async def site(update, context):
-    await update.message.reply_text(
-        "Сайт: http://www.yandex.ru/company")
-
-
-async def work_time(update, context):
-    await update.message.reply_text(
-        "Время работы: круглосуточно.")
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("address", address))
-    application.add_handler(CommandHandler("phone", phone))
-    application.add_handler(CommandHandler("site", site))
-    application.add_handler(CommandHandler("work_time", work_time))
-    application.add_handler(CommandHandler("help", help))
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("close", close_keyboard))
+
+    application.add_handler(CommandHandler(["start", "help"], start))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+
     application.run_polling()
 
 

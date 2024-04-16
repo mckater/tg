6e6102a406e85c49131c956f79d6e02de1c7c28a
izotovaee <izotova.zg@gmail.com>
2024-04-16 15:31:20 +0300

    CommandHandlers

--- main.py
+++ main.py
@@ -1,57 +1,47 @@
-# Импортируем необходимые классы.
 import logging
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram.ext import Application, CommandHandler
 from config import BOT_TOKEN
 
-# Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
 
-# Напишем соответствующие функции.
-# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
-async def start(update, context):
-    """Отправляет сообщение когда получена команда /start"""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
-    )
+
+async def help(update, context):
+    await update.message.reply_text(
+        "Я бот справочник.")
 
 
-async def help_command(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+async def address(update, context):
+    await update.message.reply_text(
+        "Адрес: г. Москва, ул. Льва Толстого, 16")
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
-async def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    await update.message.reply_text(update.message.text)
+async def phone(update, context):
+    await update.message.reply_text("Телефон: +7(495)776-3030")
+
+
+async def site(update, context):
+    await update.message.reply_text(
+        "Сайт: http://www.yandex.ru/company")
+
+
+async def work_time(update, context):
+    await update.message.reply_text(
+        "Время работы: круглосуточно.")
 
 
 def main():
-    # Создаём объект Application.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
     application = Application.builder().token(BOT_TOKEN).build()
-
-    # Сначала регистрируем команды
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # Затем регистрируем обработчик прочих (т.е.) текстовых сообщений.
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Запускаем приложение.
+    application.add_handler(CommandHandler("address", address))
+    application.add_handler(CommandHandler("phone", phone))
+    application.add_handler(CommandHandler("site", site))
+    application.add_handler(CommandHandler("work_time", work_time))
+    application.add_handler(CommandHandler("help", help))
     application.run_polling()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()

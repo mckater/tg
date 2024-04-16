918724fcb0fb870d2318f8ef4149a6a840e74b49
izotovaee <izotova.zg@gmail.com>
2024-04-16 15:33:57 +0300

    keyboard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Application, CommandHandler
 from config import BOT_TOKEN
 
@@ -7,6 +9,24 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+reply_keyboard = [['/address', '/phone'],
+                  ['/site', '/work_time']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Я бот-справочник. Какая информация вам нужна?",
+        reply_markup=markup
+    )
+
+
+async def close_keyboard(update, context):
+    await update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 
 async def help(update, context):
@@ -40,6 +60,8 @@ def main():
     application.add_handler(CommandHandler("site", site))
     application.add_handler(CommandHandler("work_time", work_time))
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("close", close_keyboard))
     application.run_polling()
 
 
